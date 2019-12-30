d9794f7c0ae738abc3c422825cc0e38d90b8d021
Justin Abrahms <justin@abrah.ms>
2019-12-30 10:28:07 -0800

    woof. doing a terrible job of committing.

--- index.js
+++ index.js
@@ -2,64 +2,53 @@ const r = require('restructure');
 const crypto = require('crypto');
 const concat = require('concat-stream');
 
+const encryptionScheme = 'aes256';
+const ivKeySize = 16;
 
-const RequestId = new r.VersionedStruct(r.uint8, {
-    0: {
-        epochSeconds: r.int32,
-        ipv4Array: new r.Array(r.uint8, 4),
-    },
-});
+const factory = exports.factory = (struct, key) => {
+    return {
+        generateRequestId: generateRequestId.bind(undefined, struct, key),
+        decodeRequestId: decodeRequestId.bind(undefined, struct, key),
+    };
+};
 
-const key = 'my test key';
-const inputEncoding = 'latin1';
-const outputEncoding = 'base64';
-const ivKeySize = 32;
-
-exports.generateRequestId = ({ epochSeconds, ipv4Array }) => {
+const generateRequestId = exports.generateRequestId = (struct, encryptionKey, payload) => {
     return new Promise((resolve, reject) => {
         const stream = new r.EncodeStream;
-        const iv = crypto.randomBytes(ivKeySize);
-        console.log('encode ivlenth: ', iv.length);
+        const iv = new Buffer(crypto.randomBytes(ivKeySize));
 
         stream.pipe(concat(function(buf) {
-            return resolve(symmetricallyEncryptBuffer(Buffer.concat([iv,buf]), iv).toString('base64'));
+            return resolve(Buffer.concat([iv,symmetricallyEncryptBuffer(buf, encryptionKey, iv)]).toString('base64'));
         }));
 
-        RequestId.encode(stream, {
-            version: 0,
-            epochSeconds,
-            ipv4Array: new Buffer([ 255, 255, 255, 255 ]),
-        });
+        struct.encode(stream, payload);
 
         return stream.end();
     });
 };
 
-const symmetricallyEncryptBuffer = exports.symmetricallyEncryptBuffer = (buffer, iv) => {
-    console.log('encrypt incoming: ', buffer, iv);
-    const cipher = crypto.createCipher('aes256', key, iv);
+const symmetricallyEncryptBuffer = exports.symmetricallyEncryptBuffer = (buffer, encryptionKey, iv) => {
+    encryptionKey = encryptionKey.padEnd(ivKeySize, '=').slice(0, ivKeySize);
+    console.log('iv: ', iv);
+    console.log('ivsize:', iv.length);
+    const cipher = crypto.createCipheriv(encryptionScheme, encryptionKey, iv);
     return Buffer.concat([cipher.update(buffer), cipher.final()]);
-    
 };
 
-const symmetricallyDecryptBuffer = exports.symmetricallyDecryptBuffer = (buffer, iv) => {
-    console.log('decrypting buffer: ', buffer, iv);
-    const decipher = crypto.createDecipher('aes256', key, iv);
+const symmetricallyDecryptBuffer = exports.symmetricallyDecryptBuffer = (buffer, encryptionKey, iv) => {
+    encryptionKey = encryptionKey.padEnd(ivKeySize, '=').slice(0, ivKeySize);
+    const decipher = crypto.createDecipheriv(encryptionScheme, encryptionKey, iv);
     return Buffer.concat([decipher.update(buffer), decipher.final()]);
 };
 
-exports.decodeRequestId = (requestId) => {
-    console.log('reqid: ', requestId);
+const decodeRequestId = exports.decodeRequestId = (struct, encryptionKey, requestId) => {
     const buffer = Buffer.from(requestId, 'base64');
-    console.log('reqid-buffer: ', buffer);
     
     const iv = buffer.slice(0, ivKeySize);
-    console.log('decode ivlenth: ', iv.length);
     const rest = buffer.slice(ivKeySize);
-    console.log('rest: ', rest);
-
-    const deciphered = symmetricallyDecryptBuffer(Buffer.from(rest, 'base64'), iv);
-    // const deciphered = symmetricallyDecryptBuffer(Buffer.from(requestId, 'base64'));
+    // const rest = buffer;
+    
+    const deciphered = symmetricallyDecryptBuffer(Buffer.from(rest, 'base64'), encryptionKey);
     const stream = new r.DecodeStream(new Buffer(deciphered));
-    return RequestId.decode(stream);
+    return struct.decode(stream);
 };
--- index.spec.js
+++ index.spec.js
@@ -1,18 +1,32 @@
-const { generateRequestId, decodeRequestId, symmetricallyEncryptBuffer, symmetricallyDecryptBuffer } = require('./');
+const { factory, generateRequestId, decodeRequestId, symmetricallyEncryptBuffer, symmetricallyDecryptBuffer } = require('./');
 const crypto = require('crypto');
+const r = require('restructure');
 
-describe('symmetric encryption', () => {
-    const iv = crypto.randomBytes(32);
+const RequestId = new r.VersionedStruct(r.uint8, {
+    0: {
+        epochSeconds: r.int32,
+        ipv4Array: new r.Array(r.uint8, 4),
+    },
+});
+
+const encryptionKey = 'my test key';
+
+describe.only('symmetric encryption', () => {
+    const iv = new Buffer(crypto.randomBytes(16));
+
     it('should actually be symmetric w/ strings', () => {
         expect(symmetricallyDecryptBuffer(
-            symmetricallyEncryptBuffer(Buffer.from('test', 'utf8'), iv),
+            symmetricallyEncryptBuffer(Buffer.from('test', 'utf8'), encryptionKey, iv),
+            encryptionKey, 
             iv
         )).toEqual(Buffer.from('test', 'utf8'));
     });
 
     it('should actually be symmetric w/ buffers', () => {
+        console.log('iv: ', iv);
         expect(symmetricallyDecryptBuffer(
-            symmetricallyEncryptBuffer(new Buffer([255]), iv),
+            symmetricallyEncryptBuffer(new Buffer([255]), encryptionKey, iv),
+            encryptionKey, 
             iv
         )).toEqual(new Buffer([255]));
     });
@@ -21,14 +35,31 @@ describe('symmetric encryption', () => {
 describe('request ids', () => {
     const epochSeconds = 1577257801;
     const ipv4Array = '255.255.255.255'.split('.').map(x => x*1);
-    const obj = { epochSeconds, ipv4Array };
+    const obj = { version: 0, epochSeconds, ipv4Array };
 
-    it.only('should be symmetric', async () => {
-        expect(decodeRequestId(await generateRequestId(obj))).toMatchObject(obj);
+    it('should be symmetric', async () => {
+        expect(decodeRequestId(RequestId, encryptionKey, await generateRequestId(RequestId, encryptionKey, obj))).toMatchObject(obj);
     });
 
     it('should be opaque when generated', async () => {
-        const reqId = await generateRequestId(obj);
-        // expect(reqId).toMatch(/^[a-zA-Z0-9]+$/);
+        const reqId = await generateRequestId(RequestId, encryptionKey, obj);
+        console.log(reqId);
+        expect(reqId).toMatch(/^[a-zA-Z0-9/=+]+$/);
+    });
+
+    it('should not be identical if called with identical data', async () => {
+        const reqId1 = await generateRequestId(RequestId, encryptionKey, obj);
+        const reqId2 = await generateRequestId(RequestId, encryptionKey, obj);
+        console.log('req1: ', reqId1);
+        console.log('req2: ', reqId2);
+        expect(reqId1).not.toEqual(reqId2);
+    });
+
+    describe('factory funciton', () => {
+        it("should make it so we don't need to pass in struct and key to underlying functions", async () => {
+            const { generateRequestId: gen, decodeRequestId: decode } = factory(RequestId, encryptionKey);
+            const reqId = await gen(obj);
+            expect(decode(await gen(obj))).toEqual(obj);
+        });
     });
 });
