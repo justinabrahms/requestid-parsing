f3a9795388cb5b97b658af7cc4273c4c94edf446
m0n0096 <LKhg23sd>
2020-03-02 15:42:44 -0800

    Handle encoding errors, moved param order. new Buffer -> Buffer.from

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const r = require('restructure');
 const crypto = require('crypto');
 const concat = require('concat-stream');
 
-const encryptionScheme = 'aes256';
+const encryptionScheme = 'aes-256-cbc';
 const ivKeySize = 16;
 const keySize = 32;
 
@@ -13,40 +13,41 @@ const factory = exports.factory = (struct, key) => {
     };
 };
 
+const symmetricallyEncryptBuffer = exports.symmetricallyEncryptBuffer = (encryptionKey, iv, buffer) => {
+    const encryptionKeyPadded = encryptionKey.padEnd(keySize, '=').slice(0, keySize);
+    const cipher = crypto.createCipheriv(encryptionScheme, encryptionKeyPadded, iv);
+    return Buffer.concat([cipher.update(buffer), cipher.final()]);
+};
+
 const generateRequestId = exports.generateRequestId = (struct, encryptionKey, payload) => {
     return new Promise((resolve, reject) => {
         const stream = new r.EncodeStream;
         const iv = Buffer.from(crypto.randomBytes(ivKeySize));
 
-        stream.pipe(concat(function(buf) {
-            return resolve(Buffer.concat([iv,symmetricallyEncryptBuffer(buf, encryptionKey, iv)]).toString('base64'));
+        stream.pipe(concat((buf) => {
+            return resolve(Buffer.concat([iv, symmetricallyEncryptBuffer(encryptionKey, iv, buf)]).toString('base64'));
         }));
 
         struct.encode(stream, payload);
+        stream.once('error', reject);
 
         return stream.end();
     });
 };
 
-const symmetricallyEncryptBuffer = exports.symmetricallyEncryptBuffer = (buffer, encryptionKey, iv) => {
-    encryptionKey = encryptionKey.padEnd(keySize, '=').slice(0, keySize);
-    const cipher = crypto.createCipheriv(encryptionScheme, encryptionKey, iv);
-    return Buffer.concat([cipher.update(buffer), cipher.final()]);
-};
-
-const symmetricallyDecryptBuffer = exports.symmetricallyDecryptBuffer = (buffer, encryptionKey, iv) => {
-    encryptionKey = encryptionKey.padEnd(keySize, '=').slice(0, keySize);
-    const decipher = crypto.createDecipheriv(encryptionScheme, encryptionKey, iv);
+const symmetricallyDecryptBuffer = exports.symmetricallyDecryptBuffer = (encryptionKey, iv, buffer) => {
+    const encryptionKeyPadded = encryptionKey.padEnd(keySize, '=').slice(0, keySize);
+    const decipher = crypto.createDecipheriv(encryptionScheme, encryptionKeyPadded, iv);
     return Buffer.concat([decipher.update(buffer), decipher.final()]);
 };
 
 const decodeRequestId = exports.decodeRequestId = (struct, encryptionKey, requestId) => {
     const buffer = Buffer.from(requestId, 'base64');
-    
+
     const iv = buffer.slice(0, ivKeySize);
     const rest = buffer.slice(ivKeySize);
-    
-    const deciphered = symmetricallyDecryptBuffer(Buffer.from(rest, 'base64'), encryptionKey, iv);
+
+    const deciphered = symmetricallyDecryptBuffer(encryptionKey, iv, Buffer.from(rest, 'base64'));
     const stream = new r.DecodeStream(Buffer.from(deciphered));
     return struct.decode(stream);
 };
